73eefe45ced78ebeb4032581b3f2c606140d6b67
Duc Tran <trananhduc1004@gmail.com>
2014-09-24 14:15:58 -0400

    Create a list of solar system names to choose from. Also create a static method to generate and return an universe (an array of 50 randomly generated solarsystem).

--- Space Trader/src/space/trader/SolarSystem.java
+++ Space Trader/src/space/trader/SolarSystem.java
@@ -4,6 +4,11 @@
  * and open the template in the editor.
  */
 package space.trader;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import java.util.ArrayList;
+import java.util.Random;
+
 
 /**
  * A class representing a solar system in the universe
@@ -12,9 +17,31 @@ package space.trader;
 
 public class SolarSystem {
     /**
+     * An enum to represent available names for the planet
+     */
+    public static enum Name {
+        ACAMAR,ADAHN, ALDEA, ANDEVIAN,ANTEDI,
+        BALOSNEE, BARATAS,BRAX, BRETEL,	BOB_WATER,
+        CALONDIA, CAMPOR, CAPELLE, CARZON, CASTOR, CESTUS, CHERON, COURTENEY,
+        DALED, DAMAST, DAVLOS, DENEB, DENEVA, DEVIDIA, DRAYLON, DUC_TRAN, DREMA,
+        ENDOR, ESMEE, EXO, FERRIS, FESTEN, FOURMI, FROLIX, GEMULON, GUINIFER,
+        HADES, HAMLET, HELENA, HULST, IODINE, IRALIUS,
+        JANUS, JAPORI, JARADA, JASON, JUSTIN_COATES, JD_ARTHUR,
+        KAYLON, KHEFKA, KIRA, KLAATU, KLAESTRON, KORMA, KRAVAT, KRIOS,
+        LAERTES, LARGO, LAVE, LIGON, LOWRY,
+        MAGRAT, MALCORIA, MELINA, MENTAR, MERIK, MINTAKA, MORDAN, MYRTHE,
+        NELVANA, NIX, NYLE, ODET, OG, OMEGA, OMPHALOS, ORIAS, OTHELLO, 
+        PARADE, PENTHARA, PICARD, POLLUX, QUATOR, QIFENG_LI,
+        RAKHAR, RAN, REGULAS, RELVA, RHYMUS, ROCHANI, RUBICUM, RUTIA,
+        SARPEIDON, SEFALLA, SELTRICE, SIGMA, SOL, SOMARI, STAKORON, STYRIS,
+        TALANI, TAMUS, TANTALOS, TANUGA, TARCHANNEN, TEROSA, THERA, TITAN,
+        TORIN, TRIACUS, TURKANA, TYRUS, UMBERLEE, UTOPIA, WEIYU_LI,
+        VADERA, VAGRA, VANDOR, VENTAX, XENON, XERXES, YEW, YOJIMBO, ZALKON
+    }
+    /**
      * An enum to represent the available resources characteristic of the planet
      */
-    public enum Resources {
+    public static enum Resources {
     NOSPECIALRESOURCES,  MINERALRICH, MINERALPOOR, DESERT,
     LOTSOFWATER, RICHSOIL, POORSOIL, RICHFAUNA, LIFELESS,
     WEIRDMUSHROOMS, LOTSOFHERBS, ARTISTIC, WARLIKE
@@ -22,30 +49,99 @@ public class SolarSystem {
     /**
      * An enum to represent the available resources characteristic of the planet
      */
-    public enum TechLevel {
+    public static enum TechLevel {
          PRE_AGRICULTURE, AGRICULTURE, MEDIEVAL, RENAISSANCE,
          EARLY_INDUSTRIAL, INDUSTRIAL, POST_INDUSTRIAL, HI_TECH
     }
-    private String name;
+    private Name name;
     private int x, y;
     private TechLevel techLevel;
     private Resources resources;
     /**
+     * Constructor of the solar system
+     */
+    public SolarSystem(Name name, int x, int y, TechLevel techLevel, Resources resources) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.techLevel = techLevel;
+        this.resources = resources;
+        
+    }
+    /**
      * Create getter for each data in the object
      */
-    public String getName() {
+    /**
+     * Return the name
+     * @return: name
+     */
+    
+    public Name getName() {
         return name;
     }
+    /**
+     * Return the horizontal location
+     * @return : x
+     */
     public int getX() {
         return x;
     }
+    /**
+     * Return the vertical location
+     * @return : y
+     */
     public int getY() {
         return y;
     }
+    /**
+     * Return the techLevel
+     * @return : techLevel
+     */
     public TechLevel getTechLevel() {
         return techLevel;
     }
+    /**
+     * Return the available resources
+     * @return : resources
+     */
     public Resources getResources() {
         return resources;
+    }
+    
+    @Override
+    public String toString() {
+        return name.name() + " " + x + " " + y + " " + techLevel.name() + " "
+                + resources.name();
+    }
+    /**
+     * Generate an universe with random generated solar systems.
+     */
+    public static ArrayList<SolarSystem> universeGenerate() {
+        // Get number of available system nam
+        int numberOfName = Name.values().length;
+        Random randObject = new Random();
+        // Generate 2 arrayList
+        ArrayList<Integer> numArray = new ArrayList();
+        ArrayList<SolarSystem> universe = new ArrayList();
+        // Generate each planet and add it to the array
+        for (int i = 0; i < 50; i++) {
+            
+            // Generate a random unique universe name
+            
+            int nextNum = randObject.nextInt(numberOfName);
+            while (numArray.contains(nextNum)) {
+                nextNum = randObject.nextInt(numberOfName);
+            }
+            Name systemName = Name.values()[nextNum];
+            // Pick tech level and resources randomly
+            TechLevel systemLevel = TechLevel.values()[randObject.nextInt(8)];
+            Resources systemResources = Resources.values()[randObject
+                                                           .nextInt(13)];
+            numArray.add(nextNum);
+            universe.add(new SolarSystem(systemName, 0, 0, systemLevel,
+                    systemResources));
+
+        }
+        return universe;
     }
 }
--- Space Trader/src/space/trader/SpaceTrader.java
+++ Space Trader/src/space/trader/SpaceTrader.java
@@ -11,6 +11,8 @@
 package space.trader;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -21,6 +23,8 @@ import javafx.stage.Stage;
  * @author Duc
  */
 public class SpaceTrader extends Application {
+    // Create the universe as a List of solar system
+    private ObservableList<SolarSystem> Universe = FXCollections.observableArrayList();
     
     @Override
     public void start(Stage stage) throws Exception {
@@ -37,6 +41,6 @@ public class SpaceTrader extends Application {
      */
     public static void main(String[] args) {
         launch(args);
-    }
+    } 
     
 }
