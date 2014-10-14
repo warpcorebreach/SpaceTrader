e1c58a1e2f9c2873de54ee85c3f57d9524a97b1e
warpcorebreach <jcoates5327@gmail.com>
2014-10-13 23:49:30 -0400

    Couple more UI adjustments
    Centered some buttons and such

--- Space Trader/src/space/trader/location/Market.java
+++ Space Trader/src/space/trader/location/Market.java
@@ -7,6 +7,7 @@ package space.trader.location;
 
 import java.util.ArrayList;
 import java.util.Random;
+import space.trader.gameplay.Data;
 import space.trader.resources.Firearms;
 import space.trader.resources.Food;
 import space.trader.resources.Furs;
