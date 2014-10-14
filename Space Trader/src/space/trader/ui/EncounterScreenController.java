dcd8a36f4bc0cfbfaba70bceb9afcfe51b177193
weiyuli <riceown@gmail.com>
2014-10-14 17:22:01 -0400

    added more encounters
    reserved spaces for extra credit

--- Space Trader/src/space/trader/ui/EncounterScreenController.java
+++ Space Trader/src/space/trader/ui/EncounterScreenController.java
@@ -30,6 +30,7 @@ public class EncounterScreenController implements Initializable {
     @FXML
     private Label encounter = new Label();
     int fuel = Data.getPlayer().getShip().getFuel();
+    int cash = Data.getPlayer().getCash();
     /**
      * Initializes the controller class.
      */
@@ -41,10 +42,30 @@ public class EncounterScreenController implements Initializable {
             encounter.setText("A fuel leak has occured on your ship. You have lost half of your fuels.");
             fuel = fuel/2;
             Data.getPlayer().getShip().setFuel(fuel);
-        } else {
-                        encounter.setText("A fuel leak has occured on your ship. You have lost half of your fuels.");
-            fuel = fuel/2;
+        } else if (encounterProb == 1) {
+            encounter.setText("You got robbed by bandits and have lost all of your cash");
+            cash = 0;
+            Data.getPlayer().setCash(cash);
+        } else if (encounterProb == 2) {
+            encounter.setText("You have discovered an abandoned SpacedShip with cash and fuel inside.");
+            fuel = fuel + 50;
+            cash = cash + 2500;
+            Data.getPlayer().setCash(cash);
             Data.getPlayer().getShip().setFuel(fuel);
+        } else if (encounterProb == 3) {
+            encounter.setText("Your ship accidently hit an Asteroid, You have to spend half of your cash repairing it.");
+            cash = cash/2;
+            Data.getPlayer().setCash(cash);
+        } else if (encounterProb == 4) {
+            encounter.setText("Your have discovered rare minerals on a nearby Asteroid. your cash increases");
+            cash = cash + 10000;
+            Data.getPlayer().setCash(cash);
+        } else if (encounterProb == 5) {
+            encounter.setText("Reserved for Pirates");
+        } else if (encounterProb == 6) {
+            encounter.setText("Reseved for Police");
+        } else if (encounterProb == 7) {
+            encounter.setText("Reserved for Merchant");
         }
     }    
     
