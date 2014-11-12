d3aaabc45334ce642e58b4f165defdfda831efea
dtran39 <trananhduc1004@gmail.com>
2014-11-12 11:53:49 -0500

    Checkstyle for 2 files

--- Space Trader/src/space/trader/resources/ShipTypes/BumbleBee.java
+++ Space Trader/src/space/trader/resources/ShipTypes/BumbleBee.java
@@ -6,7 +6,10 @@ package space.trader.resources.ShipTypes;
  * @author Weiyu
  */
 public class BumbleBee extends ShipType {
+    /**.
+     * Constructor to create a bumblebee
+     */
     public BumbleBee() {
-        super("BumbleBee",25,1,2,2,2,45,0,7,60000,1,125,15,100);
+        super("BumbleBee", 25, 1, 2, 2, 2, 45, 0, 7, 60000, 1, 125, 15, 100);
     }
 }
