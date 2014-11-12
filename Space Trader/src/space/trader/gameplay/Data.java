2bce39a1d86ebcfbe3990dd90607725b4a685a81
dtran39 <trananhduc1004@gmail.com>
2014-11-12 11:49:53 -0500

    Doing checkstyle for 2 files

--- Space Trader/src/space/trader/gameplay/Data.java
+++ Space Trader/src/space/trader/gameplay/Data.java
@@ -13,19 +13,36 @@ import space.trader.location.Shipyard;
 import space.trader.location.SolarSystem;
 import space.trader.location.Universe;
 
-/**
+/**.
  *
  * @author JD Arthur
  */
 public class Data {
+    /**. 
+     * The current data instance of the game
+     */
     private static Data instance = new Data();
+    /**. 
+     * The current player instance of the game
+     */
     private static Player currentPlayer;
+    /**. 
+     * The current solar system instance of the game
+     */
     private static SolarSystem solarSys;
+    /**. 
+     * The current universe instance of the game
+     */
     private static Universe universe;
+    /**. 
+     * The current market instance of the game
+     */
     private static Market mar;
+    /**. 
+     * The current shipyard instance of the game
+     */
     private static Shipyard shipyard;
-    
-    /**
+    /**.
      * This returns a static instance of this class so the data remains 
      * constant no matter which controller is accessing it.
      * @return Data a static instance of this class
@@ -33,20 +50,29 @@ public class Data {
     public static Data getInstance() {
         return instance;
     }
-    
+     /**.
+     * Setter method for the Data object
+     * @param in the data instance to set as current
+     */
     public static void setInstance(Data in) {
         instance = in;
     }
-    
+    /**.
+     * Setter method for the Universe object
+     * @param u the universe to set as current
+     */
     public static void setUniverse(Universe u) {
         universe = u;
     }
-    
+    /**.
+     * Getter method for the univese
+     * @return the universe of the game
+     */
     public static Universe getUniverse() {
         return universe;
     }
     
-    /**
+    /**.
      * Getter method for the Player object
      * @return the player who is currently playing
      */
@@ -54,7 +80,7 @@ public class Data {
         return currentPlayer;
     }
     
-    /**
+    /**.
      * Setter method for the Player object
      * @param p the player to set as current player
      */
@@ -63,7 +89,7 @@ public class Data {
     }
     
     
-    /**
+    /**.
      * Getter method for the current solarystem
      * @return the solar system you are currently in
      */
@@ -71,7 +97,7 @@ public class Data {
         return solarSys;
     }
     
-    /**
+    /**.
      * setter method for the current solar system
      * @param s the solarsystem you want to set as the current one
      */
@@ -79,7 +105,7 @@ public class Data {
         solarSys = s;
     }
     
-    /**
+    /**.
      * Setter method for the current market
      * @param m the market that you want to set as current
      */
@@ -87,7 +113,7 @@ public class Data {
         mar = m;
     }
     
-    /**
+    /**.
      * Getter method for the current market
      * @return the market the player is currently at
      */
@@ -95,7 +121,7 @@ public class Data {
         return mar;
     }
     
-    /**
+    /**.
      * Setter method for the current shipyard
      * @param s the shipyard that you want to set as current
      */
@@ -103,7 +129,7 @@ public class Data {
         shipyard = s;
     }
     
-    /**
+    /**.
      * Getter method for the current shipyard
      * @return the shipyard the player is currently at
      */
--- Space Trader/src/space/trader/gameplay/SpaceTrader.java
+++ Space Trader/src/space/trader/gameplay/SpaceTrader.java
@@ -15,7 +15,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+/**.
+ * Representing the game space trader
+ * @author trananhduc1004
+ */
 public class SpaceTrader extends Application {
     
     @Override
