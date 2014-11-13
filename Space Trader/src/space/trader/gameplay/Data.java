73060b3bfcb9490dee7d0b255897e5acb73068b5
warpcorebreach <jcoates5327@gmail.com>
2014-11-12 20:17:06 -0500

    More checkstyle stuff

--- Space Trader/src/space/trader/gameplay/Data.java
+++ Space Trader/src/space/trader/gameplay/Data.java
@@ -1,9 +1,9 @@
 /**
- * 
+ *
  * This class represents the model in the MVC.
- * 
- * It holds the data in one place (singleton model) so that the controllers 
- * can access data from other controllers. 
+ *
+ * It holds the data in one place (singleton model) so that the controllers
+ * can access data from other controllers.
  */
 
 package space.trader.gameplay;
@@ -13,37 +13,36 @@ import space.trader.location.Shipyard;
 import space.trader.location.SolarSystem;
 import space.trader.location.Universe;
 
-/**.
- *
- * @author JD Arthur
+/**
+ * @author JD Arthur.
  */
 public class Data {
-    /**. 
+    /**.
      * The current data instance of the game
      */
     private static Data instance = new Data();
-    /**. 
+    /**.
      * The current player instance of the game
      */
     private static Player currentPlayer;
-    /**. 
+    /**.
      * The current solar system instance of the game
      */
     private static SolarSystem solarSys;
-    /**. 
+    /**.
      * The current universe instance of the game
      */
     private static Universe universe;
-    /**. 
+    /**.
      * The current market instance of the game
      */
     private static Market mar;
-    /**. 
+    /**.
      * The current shipyard instance of the game
      */
     private static Shipyard shipyard;
     /**.
-     * This returns a static instance of this class so the data remains 
+     * This returns a static instance of this class so the data remains
      * constant no matter which controller is accessing it.
      * @return Data a static instance of this class
      */
@@ -65,13 +64,13 @@ public class Data {
         universe = u;
     }
     /**.
-     * Getter method for the univese
+     * Getter method for the universe
      * @return the universe of the game
      */
     public static Universe getUniverse() {
         return universe;
     }
-    
+
     /**.
      * Getter method for the Player object
      * @return the player who is currently playing
@@ -79,7 +78,7 @@ public class Data {
     public static Player getPlayer() {
         return currentPlayer;
     }
-    
+
     /**.
      * Setter method for the Player object
      * @param p the player to set as current player
@@ -87,16 +86,16 @@ public class Data {
     public static void setPlayer(Player p) {
         currentPlayer = p;
     }
-    
-    
+
+
     /**.
-     * Getter method for the current solarystem
+     * Getter method for the current solarsystem
      * @return the solar system you are currently in
      */
     public static SolarSystem getSolarSystem() {
         return solarSys;
     }
-    
+
     /**.
      * setter method for the current solar system
      * @param s the solarsystem you want to set as the current one
@@ -104,7 +103,7 @@ public class Data {
     public static void setSolarSystem(SolarSystem s) {
         solarSys = s;
     }
-    
+
     /**.
      * Setter method for the current market
      * @param m the market that you want to set as current
@@ -112,7 +111,7 @@ public class Data {
     public static void setMarket(Market m) {
         mar = m;
     }
-    
+
     /**.
      * Getter method for the current market
      * @return the market the player is currently at
@@ -120,7 +119,7 @@ public class Data {
     public static Market getMarket() {
         return mar;
     }
-    
+
     /**.
      * Setter method for the current shipyard
      * @param s the shipyard that you want to set as current
@@ -128,7 +127,7 @@ public class Data {
     public static void setShipyard(Shipyard s) {
         shipyard = s;
     }
-    
+
     /**.
      * Getter method for the current shipyard
      * @return the shipyard the player is currently at
@@ -136,5 +135,5 @@ public class Data {
     public static Shipyard getShipyard() {
         return shipyard;
     }
-    
+
 }
--- Space Trader/src/space/trader/gameplay/Player.java
+++ Space Trader/src/space/trader/gameplay/Player.java
@@ -6,17 +6,53 @@ package space.trader.gameplay;
  * @author Weiyu
  */
 public class Player {
-    private String name;
-    private int pilot, fighter, trader, engineer, investor, cash;
+    /**
+     * Player's name.
+     */
+    private final String name;
+    /**
+     * Player's pilot skill points.
+     */
+    private final int pilot;
+    /**
+     * Player's fighter skill points.
+     */
+    private final int fighter;
+    /**
+     * Player's trader skill points.
+     */
+    private final int trader;
+    /**
+     * Player's engineer skill points.
+     */
+    private final int engineer;
+    /**
+     * Player's investor skill points.
+     */
+    private final int investor;
+    /**
+     * Player's cash skill points.
+     */
+    private int cash;
+    /**
+     * Player's current Ship.
+     */
     private Ship s;
 
-    /*  Initializes the current player
+    /**
+     * Initializes the current player.
      *
+     * @param pname player's name
+     * @param p pilot skill points
+     * @param f fighter skill points
+     * @param t trader skill points
+     * @param e engineer skill points
+     * @param i investor skill points
      */
-    public Player(String name, int p, int f, int t, int e, int i) {
+    public Player(String pname, int p, int f, int t, int e, int i) {
         pilot = p;
         fighter = f;
-        this.name = name;
+        name = pname;
         trader = t;
         engineer = e;
         investor = i;
@@ -30,8 +66,8 @@ public class Player {
      * @return A string containing the relevant data.
      */
     public String save() {
-        return name + "," + pilot+"," + fighter + "," + trader + "," + engineer
-                + "," + investor + "," + cash;
+        return name + "," + pilot + "," + fighter + "," + trader + ","
+                + engineer + "," + investor + "," + cash;
     }
 
     public int getPilot() {
@@ -62,8 +98,8 @@ public class Player {
         return s;
     }
 
-    public void setShip(Ship s) {
-        this.s = s;
+    public void setShip(Ship ps) {
+        s = ps;
     }
 }
 
