6cf55ca87927d63c8b0de892e0bdb9d0ee38e028
warpcorebreach <jcoates5327@gmail.com>
2014-11-12 20:25:10 -0500

    Fixed checkstyle in SystemStats

--- Space Trader/src/space/trader/location/SystemStats.java
+++ Space Trader/src/space/trader/location/SystemStats.java
@@ -1,17 +1,15 @@
 
 package space.trader.location;
 
-import java.util.HashMap;
-
 /**
  *
  * @author Justin
  */
 public class SystemStats {
     //private HashMap<String, Name> names;
-    
+
     /**
-     * An enum to represent available names for the planet
+     * An enum to represent available names for the planet.
      */
     public enum Name {
         ACAMAR, ADAHN, ALDEA, ANDEVIAN,ANTEDI,
@@ -24,7 +22,7 @@ public class SystemStats {
         KAYLON, KHEFKA, KIRA, KLAATU, KLAESTRON, KORMA, KRAVAT, KRIOS,
         LAERTES, LARGO, LAVE, LIGON, LOWRY,
         MAGRAT, MALCORIA, MELINA, MENTAR, MERIK, MINTAKA, MORDAN, MYRTHE,
-        NELVANA, NIX, NYLE, ODET, OG, OMEGA, OMPHALOS, ORIAS, OTHELLO, 
+        NELVANA, NIX, NYLE, ODET, OG, OMEGA, OMPHALOS, ORIAS, OTHELLO,
         PARADE, PENTHARA, PICARD, POLLUX, QUATOR,
         RAKHAR, RAN, REGULAS, RELVA, RHYMUS, ROCHANI, RUBICUM, RUTIA,
         SARPEIDON, SEFALLA, SELTRICE, SIGMA, SOL, SOMARI, STAKORON, STYRIS,
@@ -32,38 +30,40 @@ public class SystemStats {
         TORIN, TRIACUS, TURKANA, TYRUS, UMBERLEE, UTOPIA, WEIYU_LI,
         VADERA, VAGRA, VANDOR, VENTAX, XENON, XERXES, YEW, YOJIMBO, ZALKON
     }
-    
+
     /**
-     * An enum to represent the available resources characteristic of the planet
+     * An enum to represent the available resources characteristic of the
+     * planet.
      */
     public enum Resources {
-    NOSPECIALRESOURCES, MINERALRICH, MINERALPOOR, DESERT,
-    LOTSOFWATER, RICHSOIL, POORSOIL, RICHFAUNA, LIFELESS,
-    WEIRDMUSHROOMS, LOTSOFHERBS, ARTISTIC, WARLIKE, NONE
+        NOSPECIALRESOURCES, MINERALRICH, MINERALPOOR, DESERT,
+        LOTSOFWATER, RICHSOIL, POORSOIL, RICHFAUNA, LIFELESS,
+        WEIRDMUSHROOMS, LOTSOFHERBS, ARTISTIC, WARLIKE, NONE
 }
     /**
-     * An enum to represent the available resources characteristic of the planet
+     * An enum to represent the available resources characteristic of the
+     * planet.
      */
     public enum TechLevel {
-         NONE (-1),
-         PRE_AGRICULTURE (0),
-         AGRICULTURE (1),
-         MEDIEVAL (2),
-         RENAISSANCE (3),
-         EARLY_INDUSTRIAL (4),
-         INDUSTRIAL (5),
-         POST_INDUSTRIAL (6),
-         HI_TECH (7);
-         
-         private final int techNumber;
-         
-         TechLevel(int techNumber) {
-             this.techNumber = techNumber;
-         }
-         
-         public int getTechNum() {
-             return techNumber;
-         }
+        NONE (-1),
+        PRE_AGRICULTURE (0),
+        AGRICULTURE (1),
+        MEDIEVAL (2),
+        RENAISSANCE (3),
+        EARLY_INDUSTRIAL (4),
+        INDUSTRIAL (5),
+        POST_INDUSTRIAL (6),
+        HI_TECH (7);
+
+        private final int techNumber;
+
+        TechLevel(int techNumber) {
+            this.techNumber = techNumber;
+        }
+
+        public int getTechNum() {
+            return techNumber;
+        }
     }
 
 }
